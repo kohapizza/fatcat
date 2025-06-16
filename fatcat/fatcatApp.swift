13437791ee06545d03895ea59fc1544eca6975fe
Koharu Saeki <koharu0929alexandros@gmail.com>
2025-06-16 17:02:32 +0900

    prepare for iOS

---
+++ fatcat/fatcatApp.swift
@@ -0,0 +1,17 @@
+//
+//  fatcatApp.swift
+//  fatcat
+//
+//  Created by 佐伯小遥 on 2025/06/16.
+//
+
+import SwiftUI
+
+@main
+struct fatcatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
