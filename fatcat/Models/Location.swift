2aa7f4aece1b0c4cf44943f26517dbfb78f7d214
Koharu Saeki <koharu0929alexandros@gmail.com>
2025-06-21 14:47:53 +0900

    位置方法

---
+++ fatcat/Models/Location.swift
@@ -0,0 +1,8 @@
+//
+//  Location.swift
+//  fatcat
+//
+//  Created by 佐伯小遥 on 2025/06/21.
+//
+
+import Foundation
---
+++ fatcat/Models/LocationSearchViewController.swift
@@ -0,0 +1,8 @@
+//
+//  LocationSearchViewController.swift
+//  fatcat
+//
+//  Created by 佐伯小遥 on 2025/06/21.
+//
+
+import Foundation
